48d3bab20a083974e225ffd44b101d0ca3dfedc1
Hugo Moreira <hugo.m@live.com.pt>
2025-07-10 14:32:06 +0100

    app reorganized

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, useLocation } from "wouter"; 
+import { Route, useLocation } from "wouter";
 import { useState } from "react";
 import CreateEmployeeView from "./views/CreateEmployeeView";
 import HomeView from "./views/Home";
 import AdminView from "./views/AdminView";
 import EditEmployeeView from "./views/EditEmployeeView";
-//import Footer from "./components/Footer.jsx";
+import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import LoginView from "./views/LoginView";
 import storeService from './services/storeService';
@@ -14,7 +14,7 @@ import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './utils/AuthContext';
 
 function App() {
-  const [location] = useLocation();  
+  const [location] = useLocation();
   const [isAuthenticated, setIsAuthenticated] = useState(storeService.getToken() !== null);
 
   const handleLogin = () => {
@@ -25,22 +25,22 @@ function App() {
     storeService.deleteToken();
     setIsAuthenticated(false);
   };
-  
+
   return (
-    <>
     <AuthProvider>
-      {location !== "/" && <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />}  
-      <Route exact path="/" component={LoginView} onLogin={handleLogin} />
-      <Route exact path="/home" component={HomeView} />
-      <Toaster />
       <ConfirmProvider>
-      <Route path="/Admin" component={AdminView} />
+        {location !== "/" && <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />}
+        
+        <Route exact path="/" component={LoginView} onLogin={handleLogin} />
+        <Route exact path="/home" component={HomeView} />
+        <Route path="/Admin" component={AdminView} />
+        <Route path="/CreateEmployee" component={CreateEmployeeView} />
+        <Route path="/EditEmployee/:_id" component={EditEmployeeView} />
+        
+        <Toaster />
+        <Footer />
       </ConfirmProvider>
-      <Route path="/CreateEmployee" component={CreateEmployeeView} />
-      <Route path="/EditEmployee/:_id" component={EditEmployeeView} />
-      {/*<Footer />*/}
     </AuthProvider>
-    </>
   );
 }
 
